1739ddda4c21d53337a1a74b46477d18e9ce31c8
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-11-16 17:19:39 +0000

    add initial todo app

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import types from './actionTypes'
+
+let todoId = 0
+
+export const addTodo = text => {
+  return {
+    type: types.ADD_TODO,
+    id: todoId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = filter => {
+  return {
+    type: types.SET_VISIBILITY_FILTER,
+    filter
+  }
+}
+
+export const toggleTodo = id => {
+  return {
+    type: types.TOGGLE_TODO,
+    id
+  }
+}
