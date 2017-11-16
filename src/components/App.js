97b3df6f17c7c3f72c8be9bedf99652f89a71ce4
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-11-16 17:41:07 +0000

    add semantic ui example

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,31 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import {Card} from 'semantic-ui-react'
 
 const App = () => (
   <div>
+  	<Card>
+	  	<Card.Content>
+	      <Card.Header>
+	        Matthew
+	      </Card.Header>
+	      <Card.Meta>
+	        <span className='date'>
+	          Joined in 2015
+	        </span>
+	      </Card.Meta>
+	      <Card.Description>
+	        Matthew is a musician living in Nashville.
+	      </Card.Description>
+	    </Card.Content>
+	    <Card.Content extra>
+	      <a>
+	        22 Friends
+	      </a>
+	    </Card.Content>
+  	</Card>
+
     <AddTodo />
     <VisibleTodoList />
     <Footer />
