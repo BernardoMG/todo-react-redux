de4692861992fc498c4df4a80bee9e31f5266725
Bernardo Graça <bernardo.marquesg@gmail.com>
2017-11-25 15:00:41 +0000

    Style improvement

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,13 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
-import {Card} from 'semantic-ui-react'
+import './App.css'
+
+// Tratar a div tag com o .css etc, em vez do Card, e.g. container
 
 const App = () => (
-  <div>
-  	<Card>
-	  	<Card.Content>
-	      <Card.Header>
-	        Matthew
-	      </Card.Header>
-	      <Card.Meta>
-	        <span className='date'>
-	          Joined in 2015
-	        </span>
-	      </Card.Meta>
-	      <Card.Description>
-	        Matthew is a musician living in Nashville.
-	      </Card.Description>
-	    </Card.Content>
-	    <Card.Content extra>
-	      <a>
-	        22 Friends
-	      </a>
-	    </Card.Content>
-  	</Card>
-
+  <div className='container'>
+    <h1 className='title'> My Todo List </h1>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,30 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
+import { Button, Input } from 'semantic-ui-react'
+import './addTodo.css'
 
 let AddTodo = ({ dispatch }) => {
-  let input
-
   return (
-    <div>
+    <div className='addTodoContainer'>
       <form
         onSubmit={e => {
           e.preventDefault()
+          const input = document.querySelector('input[name=todo]')
+
           if (!input.value.trim()) {
             return
           }
           dispatch(addTodo(input.value))
           input.value = ''
-        }}
-      >
-        <input
-          ref={node => {
-            input = node
-          }}
-        />
-        <button type='submit'>
+        }}>
+        <Input className='input' placeholder='Task...' name='todo' />
+        <Button className='addTodoButton' type='submit'>
           Add Todo
-        </button>
+        </Button>
       </form>
     </div>
   )
